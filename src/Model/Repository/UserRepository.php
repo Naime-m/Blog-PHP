8494715856bdc6b7ebafd6624bb051930fca7111
Naime Medjek <naimemedjek@gmail.com>
2021-11-12 16:40:58 +0100

    login submit -> erreurs persistantes

--- src/Model/Repository/UserRepository.php
+++ src/Model/Repository/UserRepository.php
@@ -6,5 +6,15 @@ use App\Model\Entity\User;
 
 class UserRepository extends Repository {
 
-    
+
+    public function getOneByEmailandPassword($email, $password)
+    {
+        $db = $this->dbConnect();
+        $query = $db->prepare('SELECT id, email, password, lastname, firstname 
+        FROM users WHERE email = $_POST[\'email\'], password = $_POST[\'password\']');
+        $query->execute(array($email,$password));
+        $user=$query->fetchAll(\PDO::FETCH_CLASS, User::class);
+        return $user;
+    }
+
 }

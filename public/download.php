c2c1bf69475f62012259ddbbccef73ea8e8e56cf
Naime Medjek <naimemedjek@gmail.com>
2021-11-26 16:08:33 +0100

    robots.txt

--- public/download.php
+++ public/download.php
@@ -1,35 +1,22 @@
 <?php
-if(isset($_GET['path']))
-{
 
-$url = $_GET['path'];
+if (isset($_GET['path'])) {
+    $url = $_GET['path'];
 
+    clearstatcache();
 
-clearstatcache();
+    if (file_exists($url)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($url).'"');
+        header('Content-Length: '.filesize($url));
+        header('Pragma: public');
 
+        flush();
 
-if(file_exists($url)) {
-
-
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="'.basename($url).'"');
-header('Content-Length: ' . filesize($url));
-header('Pragma: public');
-
-
-flush();
-
-
-readfile($url,true);
-
-
-exit();
+        readfile($url, true);
+    } else {
+        echo "Le chemin du fichier n'existe pas.";
+    }
 }
-else{
-echo "Le chemin du fichier n'existe pas.";
-}
-}
-echo "Le chemin du fichier n'est pas défini."
-
-?>
+echo "Le chemin du fichier n'est pas défini.";
